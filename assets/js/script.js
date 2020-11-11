fa79c69fc9c010fe9246583052629f8191805078
Prince Ahmed <hitprince99@gmail.com>
2020-11-11 22:54:20 +0600

    add assets

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+require('bootstrap');
+
+(function ($) {
+  const app = {
+    init: () => {
+
+    },
+  };
+
+  $(document).ready(app.init);
+
+})(jQuery);
