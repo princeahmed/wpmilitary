99bffe0f6fd7a9ac39de3337e8845f4856b63b63
Prince Ahmed <hitprince99@gmail.com>
2020-12-11 00:29:15 +0600

    add our specialities

--- home.php
+++ home.php
@@ -4,5 +4,6 @@ get_header();
 
 get_template_part('templates/sections/wpmilitary/header');
 get_template_part('templates/sections/wpmilitary/solutions');
+get_template_part('templates/sections/wpmilitary/specialities');
 
 get_footer();
---
+++ templates/sections/wpmilitary/specialities.php
@@ -0,0 +1,50 @@
+<section id="specialities">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="section-title">
+					<h2>Our Specialities</h2>
+					<h3>We help WordPress users around the world with amazing products that solve their web problems. </h3>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="speciality">
+					<i class="fas fa-3x fa-code"></i>
+					<h3>Unique Products</h3>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="speciality">
+					<i class="fas fa-3x fa-code"></i>
+					<h3>Clean & Organised Code</h3>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="speciality">
+					<i class="fas fa-3x fa-code"></i>
+					<h3>Problem Solvers</h3>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="speciality">
+					<i class="fas fa-3x fa-code"></i>
+					<h3>Dedicated Support</h3>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="speciality">
+					<i class="fas fa-3x fa-code"></i>
+					<h3>Security</h3>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="speciality">
+					<i class="fas fa-3x fa-code"></i>
+					<h3>Regular Updates</h3>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
